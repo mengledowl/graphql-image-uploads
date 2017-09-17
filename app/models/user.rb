c7aec502b9b1cdb076b568ac767c8b3163ae5d63
Matt Engledowl <mengledowl@gmail.com>
2017-09-17 13:06:20 -0500

    Remove commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
   has_attached_file :profile_image, default_url: '', preserve_files: true
   validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\z/
-
-#   before_save :set_image_file_name
-#
-#   private
-#
-#   def set_image_file_name
-#     self.image_file_name = "profile_image-#{Time.current.to_i}.jpg" if self.image_file_name == 'data'
-#   end
 end
